c121c8e0373e355aae51d5343d23fcf99c4dedc2
jyotikamboj <jyoti.jmit@gmail.com>
2015-01-05 14:32:25 +0530

    adding files

---
+++ pso-app/metadata.rb
@@ -0,0 +1,9 @@
+name             'app'
+maintainer       'Maxime Bury'
+maintainer_email 'maxime.bury@originate.com'
+license          'All rights reserved'
+description      'Installs/Configures app'
+long_description 'Installs/Configures app'
+version          '0.1.0'
+
+depends "play2", "~> 0.1.0" # add a dependency on the play2 cookbook
